09d89c7a0e5adcf55e250919df2071fa8291caa4
byi649 <byi649@aucklanduni.ac.nz>
2020-01-09 00:09:57 +1300

    Move pseudo-modding to own function

--- parse.py
+++ parse.py
@@ -226,121 +226,8 @@ def query_trade(name = None, ilvl = None, itype = None, links = None, corrupted 
 			if any(atype in proper_affix for atype in affix_types): #If proper_affix is an actual mod...
 				j['query']['stats'][0]['filters'].append({'id': proper_affix, 'value': {'min': value, 'max': 999}})
 
-		# Combine life and resists for pseudo-stats
-		total_ele_resists = 0
-		total_chaos_resist = 0
-		total_life = 0
-
-		# TODO: pseudo-attributes (including str -> life conversion), find a way to not hard-code
-		solo_resist_ids = [
-			'explicit.stat_3372524247', # Explicit fire resist
-			'explicit.stat_1671376347', # Explicit lightning resist
-			'explicit.stat_4220027924', # Explicit cold resist
-			'implicit.stat_3372524247', # Implicit fire resist
-			'implicit.stat_1671376347', # Implicit lightning resist
-			'implicit.stat_4220027924', # Implicit cold resist
-			'crafted.stat_3372524247',  # Crafted fire resist
-			'crafted.stat_1671376347',  # Crafted lightning resist
-			'crafted.stat_4220027924',  # Crafted cold resist
-		]
-
-		dual_resist_ids = [
-			'explicit.stat_2915988346', # Explicit fire and cold resists
-			'explicit.stat_3441501978', # Explicit fire and lightning resists
-			'explicit.stat_4277795662', # Explicit cold and lightning resists
-			'implicit.stat_2915988346', # Implicit fire and cold resists
-			'implicit.stat_3441501978', # Implicit fire and lightning resists
-			'implicit.stat_4277795662', # Implicit cold and lightning resists
-			'crafted.stat_2915988346',  # Crafted fire and cold resists
-			'crafted.stat_3441501978',  # Crafted fire and lightning resists
-			'crafted.stat_4277795662',  # Crafted cold and lightning resists
-		]
-
-		triple_resist_ids = [
-			'explicit.stat_2901986750', # Explicit all-res
-			'implicit.stat_2901986750', # Implicit all-res
-			'crafted.stat_2901986750',  # Crafted all-res
-		]
-
-		solo_chaos_resist_ids = [
-			'explicit.stat_2923486259', # Explicit chaos resist
-			'implicit.stat_2923486259', # Implicit chaos resist
-		]
-
-		dual_chaos_resist_ids = [
-			'crafted.stat_378817135',   # Crafted fire and chaos resists
-			'crafted.stat_3393628375',  # Crafted cold and chaos resists
-			'crafted.stat_3465022881',  # Crafted lightning and chaos resists
-		]
-
-		life_ids = [
-			'explicit.stat_3299347043', # Explicit maximum life
-			'implicit.stat_3299347043', # Implicit maximum life
-		]
-
-		combined_filters = []
-		# Solo elemental resists
-		for i in j['query']['stats'][0]['filters']:
-			if i['id'] in solo_resist_ids:
-				total_ele_resists += int(i['value']['min'])
-				combined_filters.append(i)
-
-		# Dual elemental resists
-		for i in j['query']['stats'][0]['filters']:
-			if i['id'] in dual_resist_ids:
-				total_ele_resists += 2*int(i['value']['min'])
-				combined_filters.append(i)
-
-		# Triple elemental resists
-		for i in j['query']['stats'][0]['filters']:
-			if i['id'] in triple_resist_ids:
-				total_ele_resists += 3*int(i['value']['min'])
-				combined_filters.append(i)
-
-		# Solo chaos resists
-		for i in j['query']['stats'][0]['filters']:
-			if i['id'] in solo_chaos_resist_ids:
-				total_chaos_resist += int(i['value']['min'])
-				combined_filters.append(i)
-
-		# Dual chaos resists
-		for i in j['query']['stats'][0]['filters']:
-			if i['id'] in dual_chaos_resist_ids:
-				total_chaos_resist += int(i['value']['min'])
-				total_ele_resists += int(i['value']['min'])
-				combined_filters.append(i)
-
-		# Maximum life
-		for i in j['query']['stats'][0]['filters']:
-			if i['id'] in life_ids:
-				total_life += int(i['value']['min'])
-				combined_filters.append(i)
-		
-
-		# Remove stats that have been combined into pseudo-stat
-		# Round down to nearest 10 for combined stats (off by default)
-		round = False
-		if round:
-			total_ele_resists = total_ele_resists - (total_ele_resists % 10)
-			total_chaos_resist = total_chaos_resist - (total_chaos_resist % 10)
-			total_life = total_life - (total_life % 10)
-
-		j['query']['stats'][0]['filters'] = [e for e in j['query']['stats'][0]['filters'] if e not in combined_filters]
-		
-		if total_ele_resists > 0:
-			j['query']['stats'][0]['filters'].append({'id': 'pseudo.pseudo_total_elemental_resistance', 'value': {'min': total_ele_resists, 'max': 999}})
-			print("[o] Combining the" + Fore.CYAN + f" elemental resistance " + Fore.WHITE + "mods from the list into a pseudo-parameter")
-			print("[+] Pseudo-mod " + Fore.GREEN + f"+{total_ele_resists}% total Elemental Resistance (pseudo)")
-
-		if total_chaos_resist > 0:
-			j['query']['stats'][0]['filters'].append({'id': 'pseudo.pseudo_total_chaos_resistance', 'value': {'min': total_chaos_resist, 'max': 999}})
-			print("[o] Combining the" + Fore.CYAN + f" chaos resistance " + Fore.WHITE + "mods from the list into a pseudo-parameter")
-			print("[+] Pseudo-mod " + Fore.GREEN + f"+{total_chaos_resist}% total Chaos Resistance (pseudo)")
-
-		if total_life > 0:
-			j['query']['stats'][0]['filters'].append({'id': 'pseudo.pseudo_total_life', 'value': {'min': total_life, 'max': 999}})
-			print("[o] Combining the" + Fore.CYAN + f" maximum life " + Fore.WHITE + "mods from the list into a pseudo-parameter")
-			print("[+] Pseudo-mod " + Fore.GREEN + f"+{total_life} to maximum Life (pseudo)")
+		# Turn life + resists into pseudo-mods
+		j = create_pseudo_mods(j)
 
 		# Now search for similar items, if none found remove a stat and try again. TODO: Refactor and include more vars.
 		num_stats_ignored = 0
@@ -390,6 +277,130 @@ def query_trade(name = None, ilvl = None, itype = None, links = None, corrupted 
 		res = query.json()
 		results = fetch(res)
 		return results
+
+def create_pseudo_mods(j):
+	"""
+	Combines life and resists into pseudo-mods
+
+	Returns item
+	"""
+	# Combine life and resists for pseudo-stats
+	total_ele_resists = 0
+	total_chaos_resist = 0
+	total_life = 0
+
+	# TODO: Find a way to not hard-code
+	# TODO: Support for attributes (including str->life), added phys to attacks, life regen
+	solo_resist_ids = [
+		'explicit.stat_3372524247', # Explicit fire resist
+		'explicit.stat_1671376347', # Explicit lightning resist
+		'explicit.stat_4220027924', # Explicit cold resist
+		'implicit.stat_3372524247', # Implicit fire resist
+		'implicit.stat_1671376347', # Implicit lightning resist
+		'implicit.stat_4220027924', # Implicit cold resist
+		'crafted.stat_3372524247',  # Crafted fire resist
+		'crafted.stat_1671376347',  # Crafted lightning resist
+		'crafted.stat_4220027924',  # Crafted cold resist
+	]
+
+	dual_resist_ids = [
+		'explicit.stat_2915988346', # Explicit fire and cold resists
+		'explicit.stat_3441501978', # Explicit fire and lightning resists
+		'explicit.stat_4277795662', # Explicit cold and lightning resists
+		'implicit.stat_2915988346', # Implicit fire and cold resists
+		'implicit.stat_3441501978', # Implicit fire and lightning resists
+		'implicit.stat_4277795662', # Implicit cold and lightning resists
+		'crafted.stat_2915988346',  # Crafted fire and cold resists
+		'crafted.stat_3441501978',  # Crafted fire and lightning resists
+		'crafted.stat_4277795662',  # Crafted cold and lightning resists
+	]
+
+	triple_resist_ids = [
+		'explicit.stat_2901986750', # Explicit all-res
+		'implicit.stat_2901986750', # Implicit all-res
+		'crafted.stat_2901986750',  # Crafted all-res
+	]
+
+	solo_chaos_resist_ids = [
+		'explicit.stat_2923486259', # Explicit chaos resist
+		'implicit.stat_2923486259', # Implicit chaos resist
+	]
+
+	dual_chaos_resist_ids = [
+		'crafted.stat_378817135',   # Crafted fire and chaos resists
+		'crafted.stat_3393628375',  # Crafted cold and chaos resists
+		'crafted.stat_3465022881',  # Crafted lightning and chaos resists
+	]
+
+	life_ids = [
+		'explicit.stat_3299347043', # Explicit maximum life
+		'implicit.stat_3299347043', # Implicit maximum life
+	]
+
+	combined_filters = []
+	# Solo elemental resists
+	for i in j['query']['stats'][0]['filters']:
+		if i['id'] in solo_resist_ids:
+			total_ele_resists += int(i['value']['min'])
+			combined_filters.append(i)
+
+	# Dual elemental resists
+	for i in j['query']['stats'][0]['filters']:
+		if i['id'] in dual_resist_ids:
+			total_ele_resists += 2*int(i['value']['min'])
+			combined_filters.append(i)
+
+	# Triple elemental resists
+	for i in j['query']['stats'][0]['filters']:
+		if i['id'] in triple_resist_ids:
+			total_ele_resists += 3*int(i['value']['min'])
+			combined_filters.append(i)
+
+	# Solo chaos resists
+	for i in j['query']['stats'][0]['filters']:
+		if i['id'] in solo_chaos_resist_ids:
+			total_chaos_resist += int(i['value']['min'])
+			combined_filters.append(i)
+
+	# Dual chaos resists
+	for i in j['query']['stats'][0]['filters']:
+		if i['id'] in dual_chaos_resist_ids:
+			total_chaos_resist += int(i['value']['min'])
+			total_ele_resists += int(i['value']['min'])
+			combined_filters.append(i)
+
+	# Maximum life
+	for i in j['query']['stats'][0]['filters']:
+		if i['id'] in life_ids:
+			total_life += int(i['value']['min'])
+			combined_filters.append(i)
+
+	# Remove stats that have been combined into pseudo-stat
+	# Round down to nearest 10 for combined stats (off by default)
+	round = False
+	if round:
+		total_ele_resists = total_ele_resists - (total_ele_resists % 10)
+		total_chaos_resist = total_chaos_resist - (total_chaos_resist % 10)
+		total_life = total_life - (total_life % 10)
+
+	j['query']['stats'][0]['filters'] = [e for e in j['query']['stats'][0]['filters'] if e not in combined_filters]
+	
+	if total_ele_resists > 0:
+		j['query']['stats'][0]['filters'].append({'id': 'pseudo.pseudo_total_elemental_resistance', 'value': {'min': total_ele_resists, 'max': 999}})
+		print("[o] Combining the" + Fore.CYAN + f" elemental resistance " + Fore.WHITE + "mods from the list into a pseudo-parameter")
+		print("[+] Pseudo-mod " + Fore.GREEN + f"+{total_ele_resists}% total Elemental Resistance (pseudo)")
+
+	if total_chaos_resist > 0:
+		j['query']['stats'][0]['filters'].append({'id': 'pseudo.pseudo_total_chaos_resistance', 'value': {'min': total_chaos_resist, 'max': 999}})
+		print("[o] Combining the" + Fore.CYAN + f" chaos resistance " + Fore.WHITE + "mods from the list into a pseudo-parameter")
+		print("[+] Pseudo-mod " + Fore.GREEN + f"+{total_chaos_resist}% total Chaos Resistance (pseudo)")
+
+	if total_life > 0:
+		j['query']['stats'][0]['filters'].append({'id': 'pseudo.pseudo_total_life', 'value': {'min': total_life, 'max': 999}})
+		print("[o] Combining the" + Fore.CYAN + f" maximum life " + Fore.WHITE + "mods from the list into a pseudo-parameter")
+		print("[+] Pseudo-mod " + Fore.GREEN + f"+{total_life} to maximum Life (pseudo)")
+
+	return j
 
 def choose_bad_mod(j):
 	"""
