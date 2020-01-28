847c580c6817e0d7a6b7f2041f6f682ae83961a8
Tomas Slusny <slusnucky@gmail.com>
2020-01-28 21:47:56 +0100

    Fix all issues
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- utils/input.py
+++ utils/input.py
@@ -1,19 +1,23 @@
 import time
+from queue import Queue
 from threading import Thread
 from tkinter import TclError
 import pyperclip
 
+is_keyboard_module_available = False
 try:
+    # This will raise error in case user is not running as root
     import keyboard
-    keyboard.add_hotkey('c', lambda: print())
-    keyboard.clear_all_hotkeys()
+    keyboard.add_hotkey('x', lambda: print())
+    keyboard.remove_hotkey('x')
     is_keyboard_module_available = True
 except Exception:
     is_keyboard_module_available = False
 
-
+is_pyinput_module_available = False
 if not is_keyboard_module_available:
     try:
+        # This will raise error if there is no display environment
         from pynput.keyboard import GlobalHotKeys, Controller, Key
         is_pyinput_module_available = True
     except Exception:
@@ -25,9 +29,14 @@ def get_clipboard():
 
 
 class ClipboardWatcher(Thread):
-    def __init__(self, callback, pause=0.3):
+    """
+    Watches for changes in clipboard and calls callback.
+    """
+    def __init__(self, callback, should_process, pause=0.3):
         super(ClipboardWatcher, self).__init__()
+        self.daemon = True
         self.callback = callback
+        self.should_process = should_process
         self.pause = pause
         self.stopping = False
 
@@ -38,7 +47,7 @@ class ClipboardWatcher(Thread):
             try:
                 text = get_clipboard()
 
-                if text != prev:
+                if text != prev and self.should_process():
                     self.callback(text)
 
                 prev = text
@@ -52,39 +61,81 @@ class ClipboardWatcher(Thread):
         self.stopping = True
 
 
+class HotkeyWatcher(Thread):
+    """
+    Watches for changes in hotkey queue and calls callbacks
+    """
+    def __init__(self, combination_to_function):
+        super(HotkeyWatcher, self).__init__()
+        self.daemon = True
+        self.combination_to_function = combination_to_function
+        self.stopping = False
+        self.queue = Queue()
+
+    def is_empty(self):
+        return self.queue.qsize() == 0
+
+    def push(self, hotkey):
+        self.queue.put(hotkey)
+
+    def run(self):
+        while not self.stopping:
+            try:
+                hotkey = self.queue.get()
+                self.combination_to_function[hotkey]()
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                # Do not fail
+                print("Unexpected exception occurred while handling hotkey: " + str(e))
+
+            self.queue.task_done()
+
+    def stop(self):
+        self.stopping = True
+
+
 class Keyboard:
     CLIPBOARD_HOTKEY = '<ctrl>+c'
 
     def __init__(self):
         self.combination_to_function = {}
+        self.hotkey_watcher = None
+        self.clipboard_callback = None
 
         if is_pyinput_module_available:
             self.controller = Controller()
             self.listener = None
-        elif not is_keyboard_module_available:
-            self.clipboard_watcher = ClipboardWatcher(lambda text: self.combination_to_function[Keyboard.CLIPBOARD_HOTKEY](text))
 
     def add_hotkey(self, hotkey, fun):
         self.combination_to_function[hotkey] = fun
 
     def start(self):
+        # Create hotkey watcher with all our hotkey callbacks
+        self.hotkey_watcher = HotkeyWatcher(self.combination_to_function)
+        self.clipboard_watcher = ClipboardWatcher(self.clipboard_callback, self.hotkey_watcher.is_empty)
+        self.hotkey_watcher.start()
+        combination_to_queue = {}
+
+        def to_watcher(watcher, hotkey):
+            return lambda: watcher.push(hotkey)
+
+        # Convert hotkey callbacks to proxy everything to hotkey watcher
+        for h in self.combination_to_function:
+            combination_to_queue[h] = to_watcher(self.hotkey_watcher, h)
+
         if is_keyboard_module_available:
-            for h in self.combination_to_function:
-                hotkey = h.replace('<', '').replace('>', '')
-                keyboard.add_hotkey(hotkey, self.combination_to_function[h])
+            for h in combination_to_queue:
+                keyboard.add_hotkey(h.replace('<', '').replace('>', ''), combination_to_queue[h])
         elif is_pyinput_module_available:
-            self.listener = GlobalHotKeys(self.combination_to_function)
+            self.listener = GlobalHotKeys(combination_to_queue)
+            self.listener.daemon = True
             self.listener.start()
-        else:
-            self.clipboard_watcher.start()
+
+        self.clipboard_watcher.start()
 
     def wait(self):
-        if is_keyboard_module_available:
-            keyboard.wait()
-        elif is_pyinput_module_available:
-            self.listener.join()
-        else:
-            self.clipboard_watcher.join()
+        self.hotkey_watcher.join()
 
     def write(self, string):
         if is_keyboard_module_available:
@@ -98,20 +149,20 @@ class Keyboard:
         elif is_pyinput_module_available:
             def safe_press(controller, k, press=True):
                 try:
-                    if press:
-                        controller.press(k)
-                    else:
-                        controller.release(k)
-                except ValueError:
+                    # Lazy way to try and convert special key to enum
                     k = Key[k]
-                    if press:
-                        controller.press(k)
-                    else:
-                        controller.release(k)
+                except Exception:
+                    pass
+
+                if press:
+                    controller.press(k)
+                else:
+                    controller.release(k)
 
             keys = key.split('+')
 
             if len(keys) == 2:
+                # Press first key, then second key, then release second key and finally release first key
                 safe_press(self.controller, keys[0])
                 safe_press(self.controller, keys[1])
                 safe_press(self.controller, keys[1], False)
