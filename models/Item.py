3c49eb5cfbe05f66aaa21b35bfec8a6b44c4913e
sdf010 <5693358+sdf010@users.noreply.github.com>
2020-01-31 00:18:44 +0000

    Removed unnecessary attributes from Item class
    Added function placeholders

--- models/Item.py
+++ models/Item.py
@@ -1,6 +1,6 @@
 from attr import attrs
 
-from models.item_modifier import ItemModifier
+from .item_modifier import ItemModifier
 
 
 @attrs(auto_attribs=True)
@@ -13,18 +13,7 @@ class Item:
     # TODO: handle base stats
     stats: [str] = []
 
-    # can we use a named tuple here?
-    level: str = 0  # also use for tier?
-    str: int = 0
-    dex: int = 0
-    int: int = 0
-
     raw_sockets: str = ''
-    r_sockets: int = 0
-    b_sockets: int = 0
-    g_sockets: int = 0
-    w_sockets: int = 0
-    links: int = 0
 
     modifiers: [ItemModifier] = []
     corrupted: bool = False
@@ -32,13 +21,17 @@ class Item:
     # TODO: handle influence types, as an enum?
     influence: str = ''
 
-    # optionals
-    # Some items have their class included (e.g. Rune Dagger for base Boot Blade)
-    item_class: str = ''
+    def __attrs_post_init__(self):
+        sockets = self.raw_sockets.lower()
+        self.r_sockets = sockets.count('r')
+        self.b_sockets = sockets.count('b')
+        self.g_sockets = sockets.count('g')
+        self.w_sockets = sockets.count('w')
+        # TODO: fix link count
+        self.links = sockets.count('-')
 
-    # do we need type to differentiate between item/gem/jewls/cards/currency?
-    type: str = 'Pseudo'
+    def get_pseudo_mods(self):
+        raise NotImplementedError
 
-    flavour_text: str = ''
-    instruction_text: str = ''
-    stack_size: int = 0
+    def get_json(self):
+        raise NotImplementedError
