49b58806b2bcbf3a274e364beb10ae4b6d5ed712
sdf010 <5693358+sdf010@users.noreply.github.com>
2020-02-01 01:27:50 +0000

    modified Item class based on discussions in Discord
    Item Tier is stored in iLevel, link counting is more robust and influence is now stored as a list

--- models/Item.py
+++ models/Item.py
@@ -14,12 +14,15 @@ class Item:
     stats: [str] = []
 
     raw_sockets: str = ''
+    # Item level/ Map tier
+    iLevel: int = 0
 
+    # TODO: support map IIQ, IIR, pack size
     modifiers: [ItemModifier] = []
     corrupted: bool = False
 
     # TODO: handle influence types, as an enum?
-    influence: str = ''
+    influence: [str] = []
 
     def __attrs_post_init__(self):
         sockets = self.raw_sockets.lower()
@@ -27,8 +30,9 @@ class Item:
         self.b_sockets = sockets.count('b')
         self.g_sockets = sockets.count('g')
         self.w_sockets = sockets.count('w')
+        self.a_sockets = sockets.count('a')  # abyssal
         # R-R-R-R R-R is not incorrectly registered as 5 link
-        self.links = sockets.split(' ')[0].count('-') + 1
+        self.links = sockets.count('-') - sockets.count(' ') + 1
 
     def get_pseudo_mods(self):
         raise NotImplementedError
